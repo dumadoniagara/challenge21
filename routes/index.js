f5771e5ada56e976636977bc4f86d45a083535a7
dumadoniagara <dumadsambora@gmail.com>
2020-04-25 03:38:51 +0700

    fixing date problem

--- routes/index.js
+++ routes/index.js
@@ -64,28 +64,33 @@ module.exports = (db) => {
             return res.send(`data can not be found`);
           }
           else {
-            res.render('source', { data : data.rows, page, pages });
+            res.render('source', {
+              data: data.rows,
+              page,
+              pages,
+              moment
+            });
           }
         });
       }
     });
-    
+
   });
 
   // ADD New Data
-  router.get('/add', function(req, res){
+  router.get('/add', function (req, res) {
     res.status(200).render('add')
   })
 
-  router.post('/add', function (req, res, next) {
+  router.post('/add', function (req, res) {
     db.query('INSERT INTO bread (stringdata, integerdata, floatdata, booleandata, datedata) VALUES  ($1,$2,$3,$4,$5)', [req.body.stringdata, parseInt(req.body.integerdata), parseFloat(req.body.floatdata), JSON.parse(req.body.booleandata), req.body.datedata], (err) => {
       if (err) return res.status(500).json({
         error: true,
         message: err
       })
       res.status(201).json({
-        error : false,
-        message : 'data success to be added'
+        error: false,
+        message: 'data success to be added'
       });
     });
   });
@@ -103,15 +108,35 @@ module.exports = (db) => {
   })
 
   // Edit Data refers to it's id
+  router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+
+    let sql = `SELECT * FROM bread WHERE id = ${id}`;
+    db.query(sql, (err, data) => {
+      if (err) return res.status(500).json({
+        error: true,
+        message: 'data tidak dapat diakses'
+      });
+      const { id, stringdata, integerdata, floatdata, booleandata, datedata } = data.rows[0];
+      const newDate = moment(datedata).format("YYYY-MM-DD");
+      const edit = {
+        id, stringdata, integerdata, floatdata, booleandata, newDate
+      }
+      res.render('edit', { row: edit });
+    });
+  });
+
   router.post('/edit/:id', function (req, res, next) {
-    db.query('UPDATE bread SET stringdata = $2, integerdata = $3, floatdata = $4, booleandata = $5, datedata = $6  WHERE id = $1', [parseInt(req.params.id), req.body.stringdata, parseInt(req.body.integerdata), parseFloat(req.body.floatdata), JSON.parse(req.body.booleandata), req.body.datedata], (err, data) => {
+    db.query('UPDATE bread SET stringdata = $2, integerdata = $3, floatdata = $4, booleandata = $5, datedata = $6  WHERE id = $1', [parseInt(req.params.id), req.body.stringdata, parseInt(req.body.integerdata), parseFloat(req.body.floatdata), JSON.parse(req.body.booleandata), req.body.datedata], (err) => {
       if (err) return res.status(500).json({
         error: true,
         message: err
-      })
-      res.status(201).redirect('/');
-    })
-  })
+      });
+      res.status(201).json({});
+    });
+    // const data = [parseInt(req.params.id), req.body.stringdata, parseInt(req.body.integerdata), parseFloat(req.body.floatdata), JSON.parse(req.body.booleandata), req.body.datedata];
+    res.redirect('/');
+  });
 
   return router;
 }
